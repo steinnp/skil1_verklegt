6a58e2841232e05d8930edccee40b84fc24a1629
steinnp <steiniell@gmail.com>
2014-11-27 15:45:24 +0000

    test

---
+++ skil1_verklegt/search.h
@@ -0,0 +1,14 @@
+//
+//  search.h
+//  skil1_verklegt
+//
+//  Created by Steinn Elliði Pétursson on 27/11/14.
+//  Copyright (c) 2014 Steinn Elliði Pétursson. All rights reserved.
+//
+
+#ifndef __skil1_verklegt__search__
+#define __skil1_verklegt__search__
+
+#include <stdio.h>
+
+#endif /* defined(__skil1_verklegt__search__) */
