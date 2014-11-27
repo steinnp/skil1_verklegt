0421b6839157b91fed46d186b08fcc0c2595636d
Steinn Elliði Pétursson <Steinn@Steinns-MacBook-Pro.local>
2014-11-27 13:14:58 +0000

    Initial Commit

---
+++ skil1_verklegt/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  skil1_verklegt
+//
+//  Created by Steinn Elliði Pétursson on 27/11/14.
+//  Copyright (c) 2014 Steinn Elliði Pétursson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
